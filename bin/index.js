ffc49f37b509d2c2923034f613bd4f2f82588b99
rohitmhnty <rohitkumarmahanty1234@gmail.com>
2025-05-17 18:08:44 +0530

    updated index file accordingly

--- bin/index.js
+++ bin/index.js
@@ -3,39 +3,36 @@ const arg = require('arg');
 const chalk = require('chalk');
 const getConfig = require('../src/config/config-mgr');
 const start = require('../src/commands/start');
-const build = require('../src/commands/build');
+const init = require('../src/commands/init');
+const tree = require('../src/commands/tree');
 const debug = require('debug')('bin');
-const { version } = require('../package.json');
 
 try {
   const args = arg({
     '--start': Boolean,
     '--build': Boolean,
+    '--init': String,
+    '--tree': String,
     '--help': Boolean,
     '--version': Boolean,
-    '--config': String,
   });
 
   debug('Received args', args);
 
-  if (args['--help']) {
-    usage();
-    process.exit(0);
-  }
-
-  if (args['--version']) {
-    console.log(`v${version}`);
-    process.exit(0);
-  }
-
   if (args['--start']) {
-    const config = getConfig(args['--config']);
+    const config = getConfig();
     start(config);
-  }
-
-  if (args['--build']) {
-    const config = getConfig(args['--config']);
-    build(config);
+  } else if (args['--init']) {
+    init({ projectName: args['--init'] });
+  } else if (args['--tree']) {
+    tree({ projectName: args['--tree'] });
+  } else if (args['--help']) {
+    usage();
+  } else if (args['--version']) {
+    const pjson = require('../package.json');
+    console.log(chalk.green(`Version: ${pjson.version}`));
+  } else {
+    usage();
   }
 } catch (e) {
   console.log(chalk.yellow(e.message));
@@ -44,16 +41,11 @@ try {
 }
 
 function usage() {
-  console.log(`${chalk.whiteBright('Usage: tool [OPTIONS]')}
-
-  ${chalk.greenBright('--start')}         Start the app
-  ${chalk.greenBright('--build')}         Build the app
-  ${chalk.greenBright('--help')}          Show help
-  ${chalk.greenBright('--version')}       Show version
-  ${chalk.greenBright('--config <path>')} Use custom config file
-
-  ${chalk.white('Example:')}
-  tool --start
-  tool --build --config ./tool.config.js
-  `);
+  console.log(`${chalk.whiteBright('tool [CMD]')}
+  ${chalk.greenBright('--start')}\tStarts the app
+  ${chalk.greenBright('--build')}\tBuilds the app
+  ${chalk.greenBright('--init <project-name>')}\tInitialize React app in parent directory
+  ${chalk.greenBright('--tree <project-name>')}\tShow directory tree of project in parent directory
+  ${chalk.greenBright('--help')}\tShow help
+  ${chalk.greenBright('--version')}\tShow version`);
 }
